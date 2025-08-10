3342e0146f118f9eedbad2ef17eaffcec0d1fecf
Arthur Guimarães <arthurguimaraes143@gmail.com>
2025-08-10 00:32:54 -0300

    fix: trata acentuação na listagem (closes #2)

--- src/main/java/controller/FilmeRepository.java
+++ src/main/java/controller/FilmeRepository.java
@@ -1,31 +1,51 @@
 package controller;
 
-import static org.junit.Assert.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
-import org.junit.Test;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class FilmeRepositoryTest {
+@Repository
+public class FilmeRepository {
 
-  @Test
-  public void deveListarSemErro() {
-    DriverManagerDataSource ds = new DriverManagerDataSource();
-    ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    ds.setUrl("jdbc:mysql://localhost:3306/cenaflix?useSSL=false&serverTimezone=UTC");
-    ds.setUsername("root");
-    ds.setPassword("root");
+    @Autowired
+    private JdbcTemplate jdbc;
 
-    FilmeRepository repo = new FilmeRepository();
-    repo.getClass(); // só pra evitar warning
-    // injetar template “na unha”
-    try {
-      java.lang.reflect.Field f = FilmeRepository.class.getDeclaredField("jdbc");
-      f.setAccessible(true);
-      f.set(repo, new JdbcTemplate(ds));
-    } catch (Exception e) { fail(e.getMessage()); }
+    private RowMapper<Filme> mapper = new RowMapper<Filme>() {
+        @Override
+        public Filme mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Filme f = new Filme();
+            f.setId(rs.getInt("id"));
+            f.setTitulo(rs.getString("titulo"));
+            f.setSinopse(rs.getString("sinopse"));
+            f.setGenero(rs.getString("genero"));
+            f.setAno(rs.getInt("ano"));
+            return f;
+        }
+    };
 
-    List<Filme> lista = repo.listar();
-    assertNotNull(lista);
-  }
+    public List<Filme> listar() {
+        return jdbc.query("SELECT * FROM filme ORDER BY id DESC", mapper);
+    }
+
+    public void inserir(Filme f) {
+        jdbc.update("INSERT INTO filme (titulo, sinopse, genero, ano) VALUES (?,?,?,?)",
+                f.getTitulo(), f.getSinopse(), f.getGenero(), f.getAno());
+    }
+
+    public Filme porId(int id) {
+        return jdbc.queryForObject("SELECT * FROM filme WHERE id=?", mapper, id);
+    }
+
+    public void atualizar(Filme f) {
+        jdbc.update("UPDATE filme SET titulo=?, sinopse=?, genero=?, ano=? WHERE id=?",
+                f.getTitulo(), f.getSinopse(), f.getGenero(), f.getAno(), f.getId());
+    }
+
+    public void excluir(int id) {
+        jdbc.update("DELETE FROM filme WHERE id=?", id);
+    }
 }
